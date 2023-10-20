2e29030cb2f41c68425b7187c4ebc2dae31a0d1b
Remy Saguez <resaguez.2800@gmail.com>
2023-10-20 16:36:09 +0200

    front bienvenue

--- www/src/Views/Security/login.view.php
+++ www/src/Views/Security/login.view.php
@@ -1,5 +1,8 @@
+
+<h1>Bienvenue chez</h1>
+<h2>Cultural MarketPlace</h2>
 <form>
-    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="username" placeholder="Username"></br>
     <input type="password" name="password" placeholder="Password">
     <input type="submit" value="Login">
 </form>
