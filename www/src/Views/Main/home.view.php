71aa929df6b48143e2518f43d6f420e30beba30a
Bryan DENCIL <bryan.dencil06@gmail.com>
2023-10-20 18:37:59 +0200

    homev1

---
+++ www/src/Views/Main/home.view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+     <h1>Cultural MarketPlace</h1>
+
+        <p>SEWWWWYYYYYYYYYYYYYYYY</p>
+</body>
+
+</html>
