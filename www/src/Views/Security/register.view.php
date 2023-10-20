169fac50f45d0a895e3f29505f0c6d19fbdc41a5
Bryan DENCIL <bryan.dencil06@gmail.com>
2023-10-20 17:10:32 +0200

    bryan

--- www/src/Views/Security/register.view.php
+++ www/src/Views/Security/register.view.php
@@ -1,6 +1,6 @@
 <form>
     <input type="text" name="username" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
-    <input type="password" name="passwordconf" placeholder="Password">
+    <input type="password" name="passwordconf" placeholder="Confirmation">
     <input type="submit" value="Login">
 </form>
