6b781523bb4ff491f7a20c9badb4c432c52fc642
Remy Saguez <resaguez.2800@gmail.com>
2023-10-24 20:24:50 +0200

    debut html page register

--- www/src/Views/Security/register.view.php
+++ www/src/Views/Security/register.view.php
@@ -1,6 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+<h1>Bienvenue chez</h1>
+<h2>Cultural MarketPlace</h2>
+
+<section id="section_connectez_vous">
 <form>
+    <p>Email</p>
     <input type="text" name="username" placeholder="Username">
+    <p>Mot de passe</p>
     <input type="password" name="password" placeholder="Password">
-    <input type="password" name="passwordconf" placeholder="Confirmation">
+    <p>Confirmation du mot de passe</p>
+    <input type="password" name="passwordconf" placeholder="Confirmation"><br/>
     <input type="submit" value="Login">
 </form>
+</section>
+</body>
+
+</html>
