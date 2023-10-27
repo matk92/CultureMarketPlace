5d9d2f3af44d11014da876be1a5a6b3104761a5b
Mathieu K <mathieu.keca@gmail.com>
2023-10-27 11:12:42 +0200

    modif footer

--- www/src/Views/Templates/front.tpl.php
+++ www/src/Views/Templates/front.tpl.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <title>Front</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/ba814b6b43.js" crossorigin="anonymous"></script>
 </head>
 <body>
 	
     <header>
         <div class="bg-img">
+            <div class="home-msg">Productos españoles</div>
             <nav class="container">
                 <ul>
 					<div class="nav">
@@ -26,15 +28,24 @@
         <?php include $this->view; ?>
     </main>
 
-	<footer>
-			<ul>
-				<li><a href="# ">© 2023 Cultural MarketPlace</a></li>
-				<li><a href="# ">Politique de remboursement</a></li>
-				<li><a href="# ">Politique de confidentialité</a></li>
-				<li><a href="# ">Conditions d'utilisation</a></li>
-				<li><a href="# ">Conditions générales de vente</a></li>
-			</ul>
-    </footer>
+    <footer>
+    <p>Avez-vous des préoccupations ou des questions? Soyez assuré(e) que nous sommes là pour vous aider.<br />
+       N'hésitez pas à nous contacter à l'adresse adressemail@blabla.fr, nous sommes déterminés à vous fournir une assistance rapide
+       et efficace. Votre satisfaction est notre priorité absolue.</p>  
+    <div class="social-media">
+        <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+    <hr>
+    <ul>
+        <li><a href="#">© Copyright 2023 Cultural MarketPlace</a></li>
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">Politique de confidentialité</a></li>
+        <li><a href="#">Politique de remboursement</a></li>
+        <li><a href="#">Conditions générales de vente</a></li>
+    </ul>
+</footer>
 
 </body>
 </html>
