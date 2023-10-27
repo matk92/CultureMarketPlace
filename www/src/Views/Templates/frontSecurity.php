1b5d4e10d8d852c7ed692a5a551ea52cf39c6e2b
Remy Saguez <resaguez.2800@gmail.com>
2023-10-27 18:15:29 +0200

    connection css register

---
+++ www/src/Views/Templates/frontSecurity.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Front</title>
+    <link rel="stylesheet" href="assets/css/styleConnection.css">
+    <script src="https://kit.fontawesome.com/ba814b6b43.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
