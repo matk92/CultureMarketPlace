6a76eea849dab5cbc3bb9d07a4cf2a9dd8d38222
Remy Saguez <resaguez.2800@gmail.com>
2023-10-27 18:26:29 +0200

    correction

--- www/src/Views/Templates/frontSecurity.php
+++ www/src/Views/Templates/frontSecurity.php
@@ -7,3 +7,21 @@
     <script src="https://kit.fontawesome.com/ba814b6b43.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
+
+<body>
+<main>
+        <?php include $this->view; ?>
+    </main>
+
+<script>
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  if (link.href === window.location.href) {
+    link.classList.add('active');
+  }
+});
+
+</script>
+</body>
