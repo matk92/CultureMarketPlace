fec3e4189b0de32fd4059bcd9114583a54a8d851
Bryan DENCIL <bryan.dencil06@gmail.com>
2023-11-14 11:03:11 +0100

    login html css bryan

--- www/src/Views/Security/login.view.php
+++ www/src/Views/Security/login.view.php
@@ -2,13 +2,13 @@
 <html>
 <head>
     <title>Connexion</title>
-    <link rel="stylesheet" type="text/css" href="../css/styleLogin.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/styleLogin.css">
 </head>
 
 <body>
     <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
     
-    <div  id="section_connectez_vous">
+    <div id="section_connectez_vous">
     <form>
         <div id="section_align_text">
             <h2>Connectez vous</h2>
@@ -17,7 +17,16 @@
         <p>Mot de passe</p>
         <input type="password" name="password" placeholder="Password">
         <br/><br/>
-        <button class="button" type="submit">Créer compte</button>
+        <button class="button" type="submit">Connexion</button>
+        </div>
+
+        <br/>
+        
+        <div id="information">
+        <hr />
+        <p>Mot de passe oublié ?<a href="#" class="btnconfig">cliquez ici</a></p>
+        <vl />
+        <p>Vous n'avez pas de compte ?<a& href="register" class="btnconfig">Créér un compte</a></p>
         </div>
     </form>
 </div>
