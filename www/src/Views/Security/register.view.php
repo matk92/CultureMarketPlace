6287c3d635f4c9fb2574994b9e043a024359cac0
Remy Saguez <resaguez.2800@gmail.com>
2023-11-14 08:55:33 +0100

    html register

--- www/src/Views/Security/register.view.php
+++ www/src/Views/Security/register.view.php
@@ -1,14 +1,26 @@
-<h1>Bienvenue chez</h1>
-<h2>Cultural MarketPlace</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-<section id="section_connectez_vous">
-<form>
-    <p>Email</p>
-    <input type="text" name="username" placeholder="Username">
-    <p>Mot de passe</p>
-    <input type="password" name="password" placeholder="Password">
-    <p>Confirmation du mot de passe</p>
-    <input type="password" name="passwordconf" placeholder="Confirmation"><br/>
-    <input type="submit" value="Login">
-</form>
-</section>
+<body>
+    <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
+    
+    <div  id="section_connectez_vous">
+    <form>
+        <div id="section_align_text">
+            <h2>Créer votre compte</h2>
+        <p>Email</p>
+        <input type="text" name="username" placeholder="Username">
+        <p>Mot de passe</p>
+        <input type="password" name="password" placeholder="Password">
+        <p>Confirmation du mot de passe</p>
+        <input type="password" name="passwordconf" placeholder="Confirmation"><br/><br/>
+        <button class="button" type="submit">Créer compte</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
