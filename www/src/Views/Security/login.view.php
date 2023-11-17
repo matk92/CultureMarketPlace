0ecec2d8ca1d9c41badd95bbc2181aa40808e907
Remy Saguez <resaguez.2800@gmail.com>
2023-11-17 11:13:05 +0100

    php et css du login

--- www/src/Views/Security/login.view.php
+++ www/src/Views/Security/login.view.php
@@ -5,7 +5,40 @@
     <link rel="stylesheet" type="text/css" href="../assets/css/styleLogin.css">
 </head>
 
+
 <body>
+    <div id="h1">
+        <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
+        <div  id="section_connectez_vous">
+            <form>
+                <div id="LeTableau">
+                        <h2>Connectez-vous à votre compte</h2>
+                    <div id="Email">    
+                        <p>Email</p>
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div id="MotDePasse">
+                        <p>Mot de passe</p>
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                    <br/>
+                    <button class="button" type="submit">Connexion</button>
+                </div>
+            </form>
+            <div id="information">
+                <br/>
+                <hr/>
+                    <p>Mot de passe oublié ?<a href="#" class="btnconfig"> Cliquez ici</a> | Nouveau client ?<a href="register" class="btnconfig"> C'est par ici</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
+<!--<body>
     <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
     
     <div id="section_connectez_vous">
