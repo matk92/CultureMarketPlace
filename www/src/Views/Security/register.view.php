1dd2046dc2c7aa8c03c3a08c552dd96a098ba0fd
Remy Saguez <resaguez.2800@gmail.com>
2023-11-17 10:30:19 +0100

    changement total du css register

--- www/src/Views/Security/register.view.php
+++ www/src/Views/Security/register.view.php
@@ -6,21 +6,29 @@
 </head>
 
 <body>
-    <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
-    
-    <div  id="section_connectez_vous">
-    <form>
-        <div id="section_align_text">
-            <h2>Créer votre compte</h2>
-        <p>Email</p>
-        <input type="text" name="username" placeholder="Username">
-        <p>Mot de passe</p>
-        <input type="password" name="password" placeholder="Password">
-        <p>Confirmation du mot de passe</p>
-        <input type="password" name="passwordconf" placeholder="Confirmation"><br/><br/>
-        <button class="button" type="submit">Créer compte</button>
+    <div id="h1">
+        <h1>Bienvenue chez<br><em>Cultural MarketPlace</em> </h1>
+        <div  id="section_connectez_vous">
+            <form>
+                <div id="LeTableau">
+                        <h2>Créer votre compte</h2>
+                    <div id="Email">    
+                        <p>Email</p>
+                        <input type="text" name="username" placeholder="Username">
+                    </div>
+                    <div id="MotDePasse">
+                        <p>Mot de passe</p>
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                    <div id="Confirmation">
+                        <p>Confirmation du mot de passe</p>
+                        <input type="password" name="passwordconf" placeholder="Confirmation">
+                    </div>
+                    <button class="button" type="submit">Créer compte</button>
+                </div>
+            </form>
+
         </div>
-    </form>
-</div>
+    </div>
 </body>
 </html>
